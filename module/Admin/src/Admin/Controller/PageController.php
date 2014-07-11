4d9115bd12200479e480f098211cf5bdb39148c0
wilson <gedeonpc@hotmail.com>
2014-07-10 22:48:02 -0500

    Admin Menu

--- module/Admin/src/Admin/Controller/PageController.php
+++ module/Admin/src/Admin/Controller/PageController.php
@@ -37,35 +37,35 @@ class PageController extends AbstractActionController {
      * @return type
      * @throws \Page\Controller\Exception
      */
-    /*public function dispatch(\Zend\Stdlib\RequestInterface $request, \Zend\Stdlib\ResponseInterface $response = null) {
-        $identifier = (string) $this->getEvent()->getRouteMatch()->getParam('action');
-        $identifier = "page/" . $identifier;
-        $page = $this->getPage();
+    /* public function dispatch(\Zend\Stdlib\RequestInterface $request, \Zend\Stdlib\ResponseInterface $response = null) {
+      $identifier = (string) $this->getEvent()->getRouteMatch()->getParam('action');
+      $identifier = "page/" . $identifier;
+      $page = $this->getPage();
 
-        try {
-            $page = $page->getPageByIdentifier($identifier);
+      try {
+      $page = $page->getPageByIdentifier($identifier);
 
-            // get the renderer to manipulate the title
-            $renderer = $this->getServiceLocator()->get('Zend\View\Renderer\PhpRenderer');
+      // get the renderer to manipulate the title
+      $renderer = $this->getServiceLocator()->get('Zend\View\Renderer\PhpRenderer');
 
-            // set the page title in the html head
-            $renderer->headTitle($page->title);
+      // set the page title in the html head
+      $renderer->headTitle($page->title);
 
-            // write the models content to the websites content
-            $this->layout()->content = '<h1>' . $page->title . '</h1>' . $page->content;
-        } catch (\Exception $ex) {
-            // if we are on development, show the exception,
-            // if not (we are in production) show the 404 page
-            if (isset($_SERVER['APPLICATION_ENV']) && $_SERVER['APPLICATION_ENV'] == 'development') {
-                throw $ex;
-            } else {
-                // it is necessery to call the parent dispatch, otherwise the notFoundFunction doesn't work.
-                parent::dispatch($request, $response);
-                $this->notFoundAction();
-                return;
-            }
-        }
-    }*/
+      // write the models content to the websites content
+      $this->layout()->content = '<h1>' . $page->title . '</h1>' . $page->content;
+      } catch (\Exception $ex) {
+      // if we are on development, show the exception,
+      // if not (we are in production) show the 404 page
+      if (isset($_SERVER['APPLICATION_ENV']) && $_SERVER['APPLICATION_ENV'] == 'development') {
+      throw $ex;
+      } else {
+      // it is necessery to call the parent dispatch, otherwise the notFoundFunction doesn't work.
+      parent::dispatch($request, $response);
+      $this->notFoundAction();
+      return;
+      }
+      }
+      } */
 
     public function indexAction() {
         return new ViewModel(array(
@@ -110,6 +110,7 @@ class PageController extends AbstractActionController {
                 $page->title = $request->getPost("titulo");
                 $page->content = $request->getPost("contenido");
                 $page->url = $request->getPost("url");
+                $page->modified = date("Y-m-d H:i:s");
 
                 // Guardamos los datos
                 $pageTable->savePage($page);
