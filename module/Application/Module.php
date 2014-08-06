8064696fa708c9a2fdcf3f0f66561ca0e37915c5
wilson <gedeonpc@hotmail.com>
2014-08-05 20:46:55 -0500

    Clase 7.1

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -9,6 +9,10 @@
 
 namespace Application;
 
+use Zend\Permissions\Acl\Acl;
+use Zend\Permissions\Acl\Role\GenericRole;
+use Zend\Permissions\Acl\Resource\GenericResource;
+
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
@@ -37,21 +41,127 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
         $alias = $sm->get('Application\Router\Alias');
         $nodeTable = $sm->get('Smeagol\Model\NodeTable');
         $alias->setNodeTable($nodeTable);
-   
+
         $moduleRouteListener = new ModuleRouteListener();
         $moduleRouteListener->attach($eventManager);
         $eventManager->attach('route', function($e) {
+            // rol por defecto
+            $userRole = 'guest';
+
             // verificando si el usuario esta logueado
             $auth = new AuthenticationService();
             $is_login = false;
             if ($auth->hasIdentity()) {
                 $is_login = true;
+                $identity = $auth->getIdentity();
+                $userRole = $identity->role_type;
+            }
+
+            // Datos se obtendrán de la base de datos
+            $roles = array('guest', 'member', 'editor', 'admin');
+            $permissions = array(
+                'admin' => array('mvc:admin.*', 'mvc:application.*'),
+                'guest' => array('mvc:application.*'),
+                'editor' => array('mvc:application.*', 'mvc:admin.index.index', 'mvc:admin.pages.*'),
+                'member' => array(
+                    'mvc:application.*',
+                    'mvc:admin.index.index',
+                    'mvc:admin.pages.index',
+                    'mvc:admin.pages.add',
+                    'mvc:admin.pages.edit:owner',
+                    'mvc:admin.pages.delete:owner',
+                )
+            );
+
+            // Instanciando la clase Acl
+            $acl = new Acl();
+
+            // Agregando los roles
+            foreach ($roles as $role) {
+                $acl->addRole(new GenericRole($role));
+            }
+
+            // Obteniendo los módulos del sistema
+            $manager = $e->getApplication()->getServiceManager()->get('ModuleManager');
+            $modules = $manager->getLoadedModules();
+            $loadedModules = array_keys($modules);
+            $skipActionsList = array('notFoundAction', 'getMethodFromAction');
+
+            foreach ($loadedModules as $loadedModule) {
+                $moduleClass = '\\' . $loadedModule . '\Module';
+                $moduleObject = new $moduleClass;
+                $config = $moduleObject->getConfig();
+
+                if (isset($config['controllers']['invokables'])) {
+                    $controllers = $config['controllers']['invokables'];
+                    foreach ($controllers as $key => $moduleClass) {
+                        $tmpArray = get_class_methods($moduleClass);
+                        $controllerActions = array();
+
+                        $rs = explode("\\", $moduleClass);
+                        $module = strtolower($rs[0]);
+                        $controller = strtolower($rs[2]);
+                        $controller = substr($controller, 0, -10);
+
+                        foreach ($tmpArray as $action) {
+                            if (substr($action, strlen($action) - 6) === 'Action' && !in_array($action, $skipActionsList)) {
+                                $action = strtolower(substr($action, 0, -6));
+                                $resource = "mvc:$module.$controller.$action";
+                                // agregando todos los actions como recursos de la aplicación
+                                $acl->addResource(new GenericResource($resource));
+
+                                // recorriendo el array de permisos para asignar acceso a los recursos
+                                foreach ($permissions as $role => $permission) {
+                                    $is_allowed = false;
+                                    if (in_array("mvc:$module.*", $permission)) {
+                                        $is_allowed = true;
+                                    }
+
+                                    if (in_array("mvc:$module.$controller.*", $permission)) {
+                                        $is_allowed = true;
+                                    }
+
+                                    if (in_array($resource, $permission)) {
+                                        $is_allowed = true;
+                                    }
+
+                                    // asignado permiso de acceso al recurso
+                                    if ($is_allowed) {
+                                        $acl->allow($role, $resource);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // validamos si el rol tiene acceso a este recurso
+            $routeParams = $e->getRouteMatch()->getParams();
+            $scontroller = trim($routeParams['controller']);
+            $rs = explode("\\", $scontroller);
+            $module = strtolower($rs[0]);
+            $controller = strtolower($rs[2]);
+            $action = strtolower($routeParams['action']);
+
+            $acceso = false;
+
+            // verificación de acceso al recurso
+            $resource = "mvc:$module.$controller.$action";
+            if ($acl->isAllowed($userRole, $resource)) {
+                $acceso = true;
+            }
+
+            // Bloqueo de acceso
+            if (!$acceso) {
+                die("acceso denegado");
             }
 
             // validamos si entramos en el index del portal
             $is_front = false;
             // obtenemos la ruta del request
             $ruta = $e->getRouter()->getRequestUri()->getPath();
+
             if ($ruta == "/" || $ruta == "/application" || $ruta === "/application/index" || $ruta === "/application/index/index") {
                 $is_front = true;
             }
@@ -61,7 +171,7 @@ class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
             $e->getViewModel()->setVariable("is_login", $is_login);
             $e->getViewModel()->setVariable("is_front", $is_front);
         });
-    }
+    }   
 
     public function getConfig() {
         return include __DIR__ . '/config/module.config.php';
