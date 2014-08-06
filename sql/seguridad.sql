1e9ecf0b991694ed2c20812569e2404b0a77a38e
wilson <gedeonpc@hotmail.com>
2014-08-05 20:58:36 -0500

    Clase 7.2

--- sql/seguridad.sql
+++ sql/seguridad.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-08-2014 a las 19:16:07
--- Versión del servidor: 5.5.36
--- Versión de PHP: 5.4.27
+-- Tiempo de generaciï¿½n: 05-08-2014 a las 19:16:07
+-- Versiï¿½n del servidor: 5.5.36
+-- Versiï¿½n de PHP: 5.4.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -24,7 +24,8 @@ SET time_zone = "+00:00";
 
 --
 -- Estructura de tabla para la tabla `menu`
---
+--  
+
 
 CREATE TABLE IF NOT EXISTS `menu` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -48,11 +49,11 @@ INSERT INTO `menu` (`id`, `name`, `label`, `url`, `parent_id`, `order_id`, `node
 (3, NULL, 'Inicio', '/', 1, 0, NULL),
 (4, NULL, 'Nosotros', NULL, 1, 1, 1),
 (5, NULL, 'Servicios', NULL, 1, 2, 5),
-(6, NULL, 'Programación web', NULL, 5, 0, 4),
+(6, NULL, 'Programaciï¿½n web', NULL, 5, 0, 4),
 (7, NULL, 'Desarrollo de Portales', NULL, 5, 1, 6),
 (8, NULL, 'Soluciones', NULL, 1, 3, 7),
 (9, NULL, 'Soporte', NULL, 1, 4, 8),
-(10, NULL, 'Contáctenos', NULL, 1, 5, 9),
+(10, NULL, 'Contï¿½ctenos', NULL, 1, 5, 9),
 (11, NULL, 'Dashboard', 'admin', 2, 0, NULL),
 (12, NULL, 'Contenido', 'admin/page', 2, 1, NULL),
 (13, NULL, 'Noticias', 'admin/noticias', 2, 2, NULL),
@@ -92,14 +93,14 @@ INSERT INTO `node` (`id`, `title`, `content`, `url`, `created`, `modified`, `nod
 (1, 'Nosotros', '<p><strong>Smeagol CMS, un</strong><strong>&nbsp;demo de desarrolado en Zend Framework 2</strong></p>\r\n\r\n<p>Nosotros somos hinchas de Zend Framework 2</p>\r\n\r\n<p>Gurus del php&nbsp;</p>\r\n\r\n<p>que deasrrollamos aplicaciones alucinantes</p>\r\n', 'nosotros', '2014-07-01 20:47:24', '2014-07-17 13:56:17', 1, 1),
 (2, 'Smeagol primer CMS en ZF2', 'haber si funca', 'noticias/smeagol-primer-cms-en-zf2', '2014-07-01 20:47:24', NULL, 2, 1),
 (3, 'El mundial Brasil 2014 esta que quema', 'El mundial esta super emocionante', 'noticias/mundialsuper-emocionante', '2014-07-01 20:47:24', NULL, 2, 1),
-(4, 'Programación Web', '<p>Somos unos tigres del PHP y dem&aacute;s hierbas</p>\r\n', 'servicios/programacion-web', '2014-07-10 22:47:08', '2014-07-17 13:50:46', 1, 1),
+(4, 'Programaciï¿½n Web', '<p>Somos unos tigres del PHP y dem&aacute;s hierbas</p>\r\n', 'servicios/programacion-web', '2014-07-10 22:47:08', '2014-07-17 13:50:46', 1, 1),
 (5, 'Servicios', '<p>Somos Expertos Programadores y le ofrecemos los siguientes servicios</p>\r\n\r\n<p><a href="/servicios/programacion-web">Programaci&oacute;n web</a>&nbsp;</p>\r\n\r\n<p><a href="/servicios/desarrollo-de-portales">Desarrollo de Portales</a></p>\r\n', 'servicios', '2014-07-17 13:50:18', '2014-07-17 13:50:18', 1, 1),
 (6, 'Desarrollo de Portales', '<p>Creamos portales con smeagol y drupal&nbsp;</p>\r\n', 'servicios/desarrollo-de-portales', '2014-07-17 13:52:35', '2014-07-17 13:52:35', 1, 1),
 (7, 'Soluciones', '<p><img alt="" src="/files/fotos/puesta-de-sol.jpg" style="float:left; height:141px; width:188px" />Hacemos desarrollo de software a medida y contamos con las siguientes soluciones</p>\r\n\r\n<p><a href="/soluciones/smeagolcms">Smeagol CMS</a></p>\r\n\r\n<p><a href="/soluciones/intranets">Intranets</a></p>\r\n', 'soluciones/intranets', '2014-07-17 13:54:35', '2014-08-03 19:41:00', 1, 1),
 (8, 'Soporte', '<p>Brindamos el mejor soporte al mejor precio</p>\r\n\r\n<p>en planes</p>\r\n\r\n<p>8x5</p>\r\n\r\n<p>24x7</p>\r\n', 'soporte', '2014-07-17 13:58:17', '2014-07-17 13:58:17', 1, 1),
 (9, 'Contactenos', '<p>Cont&aacute;ctenos y no se arrepentir&aacute;</p>\r\n\r\n<p>al fono &nbsp;666-666-666</p>\r\n', 'contactenos', '2014-07-17 13:59:13', '2014-07-17 13:59:13', 1, 1),
 (10, 'Smeagol', '<h1>El mejor CMS en Zend Frammework 2  :D</h1>', 'servicios/desarrollo-de-portales/smeagol', '2014-07-22 08:05:00', NULL, 1, 1),
-(11, 'Drupal 7', '<h1> El Mejor CMS del mundo</h1>\r\n<h2> Que lo usa hasta el tío Obama<h2>', 'servicios/desarrollo-de-portales/drupal', '2014-07-22 08:05:00', NULL, 1, 1);
+(11, 'Drupal 7', '<h1> El Mejor CMS del mundo</h1>\r\n<h2> Que lo usa hasta el tï¿½o Obama<h2>', 'servicios/desarrollo-de-portales/drupal', '2014-07-22 08:05:00', NULL, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -152,7 +153,7 @@ CREATE TABLE IF NOT EXISTS `role` (
 INSERT INTO `role` (`type`, `description`) VALUES
 ('admin', 'Usuarios Administradores, todos los permisos'),
 ('editor', 'Usuarios con el rol de editor, por defecto puede crear, editar y borrar todos los contenidos'),
-('guest', 'Usuarios anónimos, por defecto pueden acceder a todos los contenidos'),
+('guest', 'Usuarios anï¿½nimos, por defecto pueden acceder a todos los contenidos'),
 ('member', 'Usuarios Autenticados, por defecto pueden editar su propio contenido');
 
 -- --------------------------------------------------------
