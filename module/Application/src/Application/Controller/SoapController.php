6b0cf58d04d8aad9ca0f018ca4b674fa8a25cb26
wilson <gedeonpc@hotmail.com>
2014-08-07 21:20:13 -0500

    Clase 8

--- module/Application/src/Application/Controller/SoapController.php
+++ module/Application/src/Application/Controller/SoapController.php
@@ -9,6 +9,7 @@ use Zend\Soap\AutoDiscover;
 use Smeagol\Service\Hello;
 
 class SoapController extends AbstractActionController {
+
     public function nowsdlAction() {
         $hello = new Hello();
         $options = array('location' => 'http://vm-0.genenior2009.koding.kd.io/soap/nowsdl',
@@ -33,6 +34,40 @@ class SoapController extends AbstractActionController {
         $client = new Client(null, $options);
         
         // Invocando al método remoto sayHello
+        echo $client->sayHello("Mundo!");
+        return $this->getResponse();
+    }
+
+    // método que genera el archivo wsdl
+    public function autodiscoverWsdlAction() {
+        $autodiscover = new AutoDiscover();
+        // definiendo la clase para generar su wsdl y el 
+        // enlace del soap server
+        $autodiscover->setClass("\Smeagol\Service\Hello")
+                ->setUri('http://vm-0.genenior2009.koding.kd.io/soap/withwsdl');
+        // Se imprime el XML del descriptor WSDL
+        $autodiscover->handle();
+        // se deshabilita la vista
+        return $this->getResponse();
+    }
+    
+    public function withWsdlAction() {
+        // La instancia del soap server tiene que hacerse con el url del archivo wsdl
+        $server = new Server("http://vm-0.genenior2009.koding.kd.io/soap/autodiscoverwsdl");
+        // Se define la clase desplegada en el web service
+        $server->setClass("\Smeagol\Service\Hello");
+        // Se despliega el web service
+        $server->handle();
+        return $this->getResponse();
+    }
+
+
+    public function test2Action() {
+        $options = array('location' => 'http://vm-0.genenior2009.koding.kd.io/soap/withwsdl',
+            'uri' => 'http://vm-0.genenior2009.koding.kd.io/soap/withwsdl');
+        // Se instancia el cliente con el enlace del descriptor wsdl
+        $client = new Client('http://vm-0.genenior2009.koding.kd.io/soap/autodiscoverwsdl', $options);
+        // se invoca al método remoto
         echo $client->sayHello("Mundo!");
         return $this->getResponse();
     }
